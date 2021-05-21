28e820856a3a03988b10b38e44a9880df399ad46
Oskar van Velden <oskar@rakso.nl>
2021-05-21 19:47:33 +0200

    Make preview a bit transparent

--- entities/rooms/preview_room.gd
+++ entities/rooms/preview_room.gd
@@ -1,12 +1,9 @@
 extends "res://entities/rooms/base_room.gd"
 
 
-func _ready() -> void:
-	self_modulate = Color(1, 1, 1, 0.4)
-	
-	print("preview room")
-
-
 func set_configuration(room_config):
 	# clear door
 	_create_doors(room_config)
+	
+	for d in doors_layer.get_children():
+		d.self_modulate = Color(1, 1, 1, 0.6)
