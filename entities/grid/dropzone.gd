971456d17835a5635995367266751e1923e6b0d1
Oskar van Velden <oskar@rakso.nl>
2021-05-21 10:12:43 +0200

    Remove disabled color

--- entities/grid/dropzone.gd
+++ entities/grid/dropzone.gd
@@ -21,9 +21,6 @@ func _draw() -> void:
 	
 	if not hidden:
 		draw_rect(Rect2(0.0, 0.0, 1.0, 1.0), Color.whitesmoke)
-		
-	if disabled:
-		draw_rect(Rect2(0.0, 0.0, 16.0, 16.0), Color.red * Color(1,1,1,0.2))
 
 func hide_indicator() -> void:
 	hidden = true
