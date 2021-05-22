e43284e38201d2f7271cc73673170dc6fe2de66d
Oskar van Velden <oskar@rakso.nl>
2021-05-22 23:41:30 +0200

    Set random room-type

--- entities/rooms/suppy_room.gd
+++ entities/rooms/suppy_room.gd
@@ -10,9 +10,30 @@ func _ready() -> void:
 	$ClickArea.connect('mouse_exited', self, '_on_mouse_out')
 	$ClickArea.connect('input_event', self, '_on_mouse_event')
 	
+	_make_a_room()
+
+
+func _make_a_room():
+	var doors = _make_random_doors()
+	
+	var room_types = [RoomTypes.EMPTY, RoomTypes.ENEMY, RoomTypes.TREASURE, RoomTypes.BIG_TREASURE]
+	
+	if doors.size() == 1:
+		if randf() > 0.8:
+			room_types.append(RoomTypes.BIG_TREASURE)
+			
+		room_types.append(RoomTypes.TREASURE)
+		
+	if doors.size() == 2:
+		room_types.append(RoomTypes.TREASURE)
+		room_types.append(RoomTypes.ENEMY)
+		room_types.append(RoomTypes.HEAL)
+		
+	room_types.shuffle()
+	
 	set_configuration({
-		"doors": _make_random_doors(),
-		"type": EMPTY
+		"doors": doors,
+		"type": room_types.pop_front()
 	})
 
 
